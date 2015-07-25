127d6c51ae3142d5f3b71f92f092df06b3229a94
David Persson <davidpersson@gmx.de>
2015-07-25 16:15:29 +0200

    More vu.

--- data/upgrade/003-drop-vu.sql
+++ data/upgrade/003-drop-vu.sql
@@ -1,5 +1,6 @@
-ALTER TABLE `billing_invoices` DROP `virtual_user_id`;
-ALTER TABLE `billing_invoices` CHANGE `user_id` `user_id` INT(11)  UNSIGNED  NOT NULL;
-ALTER TABLE `billing_invoice_positions` DROP `virtual_user_id`;
-ALTER TABLE `billing_invoice_positions` CHANGE `user_id` `user_id` INT(11)  UNSIGNED  NOT NULL;
+-- After running migration command.
+-- ALTER TABLE `billing_invoices` DROP `virtual_user_id`;
+-- ALTER TABLE `billing_invoices` CHANGE `user_id` `user_id` INT(11)  UNSIGNED  NOT NULL;
+-- ALTER TABLE `billing_invoice_positions` DROP `virtual_user_id`;
+-- ALTER TABLE `billing_invoice_positions` CHANGE `user_id` `user_id` INT(11)  UNSIGNED  NOT NULL;
 

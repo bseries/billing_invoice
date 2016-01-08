ddf9f2165328140eda313a11d5174b53a6d3e0f6
David Persson <davidpersson@gmx.de>
2016-01-08 15:39:13 +0100

    Move migration into billing_payments.

--- data/upgrade/005-auto-pay.sql
+++ data/upgrade/005-auto-pay.sql
@@ -1,4 +1,1 @@
 ALTER TABLE `users` ADD `is_auto_paying` TINYINT(1)  UNSIGNED  NOT NULL  DEFAULT '0'  COMMENT 'billing' AFTER `is_auto_invoiced`;
-ALTER TABLE `users` ADD `payment_method` VARCHAR(100)  NULL  DEFAULT NULL  AFTER `tax_no`;
-
-

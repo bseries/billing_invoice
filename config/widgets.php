14165a84e20e4b6650375cbd6c90432ddb797c10
Marius Wilms <mariuswilms@mailbox.org>
2018-06-08 15:12:24 +0200

    Fix sorting and refactor top unbilled users calculation

--- config/widgets.php
+++ config/widgets.php
@@ -59,60 +59,11 @@ Widgets::register('invoices', function() use ($t) {
 Widgets::register('topUnbilledUsers', function() use ($t) {
 	$formatter = new MoneyFormatter(Environment::get('locale'));
 
-	$data = [];
-
-	$grouped = InvoicePositions::find('all', [
-		'group' => ['user_id'],
-		'order' => ['count_positions' => 'DESC', 'created' => 'DESC'],
-		'fields' => [
-			// FIXME Currently does not calculate value but number
-			// of positions as amount calculation is done app side.
-			'COUNT(*) AS count_positions', 'user_id', 'User.id'
-		],
-		'conditions' => [
-			'billing_invoice_id' => null,
-			// Ignore all users that have been deleted in the meantime.
-			'User.id' => ['IS NOT' => null]
-		],
-		'with' => ['User'],
-		'limit' => 10
-	]);
-	$userIds = Set::extract(array_values($grouped->data()), '/user/id');
-
-	if ($userIds) {
-		$positions = InvoicePositions::find('all', [
-			'conditions' => [
-				'billing_invoice_id' => null,
-				'user_id' => $userIds
-			],
-			'order' => [
-				'field(user_id, ' . implode(',', $userIds) . ')',
-				'created' => 'DESC'
-			],
-			'fields' => [
-				'id', 'user_id', 'User.number', 'User.id', 'User.name',
-				'quantity',
-				'amount', 'amount_type', 'amount_currency', 'amount_rate'
-			],
-			'with' => ['User']
-		]);
-
-		foreach ($positions as $position) {
-			$user = $position->user();
-			$name = $user->number . ' / ' . $user->name;
-
-			if (!isset($data[$name])) {
-				$data[$name] = $position->total();
-			} else {
-				$data[$name] = $data[$name]->add($position->total());
-			}
-		}
-	}
 	return [
 		'title' => $t('Top unbilled users'),
 		'data' => array_map(function($v) use ($formatter) {
 			return $formatter->format($v->getNet());
-		}, $data),
+		}, InvoicePositions::topUnbilledUsers()),
 		'url' => [
 			'library' => 'billing_invoice',
 			'controller' => 'InvoicePositions',
